6209565841e4bf58530110352d3b7bb1b384f15e
kamecha <masakikameyama81@gmail.com>
2023-05-04 03:57:32 +0900

    tabbyからタブの名前を取得できるようになった

--- denops/@ddu-sources/tab.ts
+++ denops/@ddu-sources/tab.ts
@@ -2,7 +2,7 @@ import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v2.5.0/types.ts";
 import { ActionData } from "../@ddu-kinds/tab.ts";
 import { Denops } from "https://deno.land/x/ddu_vim@v2.5.0/deps.ts";
 import * as fn from "https://deno.land/x/denops_std@v4.0.0/function/mod.ts";
-import { ensureArray } from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
+import { ensureArray, ensureString } from "https://deno.land/x/unknownutil@v2.0.0/mod.ts";
 
 type Params = Record<never, never>;
 
@@ -11,6 +11,18 @@ type TabInfo = {
   variables: Record<string, unknown>;
   windows: number[];
 };
+
+// ↓luaでこれを書くとtabの名前が取れる
+/*
+lua << EOF
+local tab = require('tabby.tab')
+print(tab.get_name(1))
+EOF
+*/
+async function getTabName(denops: Denops, tabnr: number): Promise<string> {
+  const tabName = await fn.execute(denops, `lua print( require('tabby.tab').get_name(${tabnr}) )`);
+  return ensureString(tabName);
+}
 
 export class Source extends BaseSource<Params> {
   kind = "tab"
@@ -23,8 +35,9 @@ export class Source extends BaseSource<Params> {
         const tabinfo = ensureArray<TabInfo>(await fn.gettabinfo(args.denops));
         const items: Item<ActionData>[] = [];
         for (const tab of tabinfo) {
+          const tabName = await getTabName(args.denops, tab.tabnr);
           items.push({
-            word: `tab ${tab.tabnr}`,
+            word: `tab ${tab.tabnr} ${tabName}`,
             action: {
               tabnr: tab.tabnr,
             }
